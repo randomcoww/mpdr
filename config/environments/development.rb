6e55eb6dac736a35ed27395096ad9cf1210b90f9
randomcoww <etherblimp@gmail.com>
2017-02-04 03:06:53 -0800

    update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,6 +39,5 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.mpd_internal_path = Rails.root.join('/tmp', 'mpd_internal')
   config.mpd_mount_path = File.join('/auto', 'music')
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,4 +39,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.mpd_mount_path = Rails.root.join('spec', 'files', 'mpd_mount')
 end
